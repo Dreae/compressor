913d9bbc688c45b5a3be992ec606e294cd4e43f5
Dreae <dreae@dreae.onl>
2019-03-02 14:14:46 -0500

    Initial

---
+++ src/compressor_filter_kern.c
@@ -0,0 +1,74 @@
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/in.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/udp.h>
+#include <linux/bpf_common.h>
+
+#include <stdint.h>
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+#define htons(x) ((__be16)___constant_swab16((x)))
+static void *(*bpf_map_lookup_elem)(void *map, void *key) = (void *) BPF_FUNC_map_lookup_elem;
+
+struct vlan_hdr {
+	__be16 h_vlan_TCI;
+	__be16 h_vlan_encapsulated_proto;
+};
+
+struct bpf_map_def {
+      unsigned int type;
+      unsigned int key_size;
+      unsigned int value_size;
+      unsigned int max_entries;
+      unsigned int map_flags;
+};
+
+struct bpf_map_def SEC("maps") blocked_ips = {
+    .type = BPF_MAP_TYPE_PERCPU_ARRAY,
+    .key_size = sizeof(uint32_t),
+    .value_size = sizeof(uint8_t),
+    .max_entries = 1000000,
+};
+
+SEC("xdp_prog")
+int xdp_program(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+
+    uint64_t nh_off = sizeof(*eth);
+    if (data + nh_off > data_end) {
+        return XDP_PASS;
+    }
+
+    uint16_t h_proto = eth->h_proto;
+    for (int i = 0; i < 2; i++) {
+		if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
+			struct vlan_hdr *vhdr;
+
+			vhdr = data + nh_off;
+			nh_off += sizeof(struct vlan_hdr);
+			if (data + nh_off > data_end) {
+				return XDP_PASS;
+			}
+			h_proto = vhdr->h_vlan_encapsulated_proto;
+		}
+    }
+
+    if (h_proto == htons(ETH_P_IP)) {
+        struct iphdr *iph = data + nh_off;
+        struct udphdr *udph = data + nh_off + sizeof(struct iphdr);
+        if (udph + 1 > (struct udphdr *)data_end) {
+            return XDP_PASS;
+        }
+
+        uint8_t *value = bpf_map_lookup_elem(&blocked_ips, &iph->saddr);
+        if (value) {
+            return XDP_DROP;
+        }
+    }
+
+    return XDP_PASS;
+}
---
+++ src/compressor_filter_user.c
@@ -0,0 +1,89 @@
+#include <linux/bpf.h>
+#include <linux/bpf_common.h>
+#include <linux/if_link.h>
+#include <net/if.h>
+#include <sys/resource.h>
+#include <libconfig.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <libbpf.h>
+#include <bpf.h>
+
+
+static int ifindex;
+
+int load_xdp_prog(const char *dir) {
+    char filename[128];
+    snprintf(filename, sizeof(filename), "%s_kern.o", dir);
+
+    struct bpf_prog_load_attr prog_load_attr = {
+        .prog_type = BPF_PROG_TYPE_XDP,
+    };
+    prog_load_attr.file = filename;
+
+    struct bpf_object *obj;
+    int prog_fd;
+    if (bpf_prog_load_xattr(&prog_load_attr, &obj, &prog_fd)) {
+        fprintf(stderr, "Error loading XDP program");
+        return 1;
+    }
+
+    struct bpf_map *map;
+    map = bpf_map__next(NULL, obj);
+    if (!map) {
+        fprintf(stderr, "Error finding map in XDP program\n");
+        return 1;
+    }
+    
+    int map_fd = bpf_map__fd(map);
+    if (!prog_fd) {
+        perror("load_bpf_file");
+        return 1;
+    }
+
+    if (bpf_set_link_xdp_fd(ifindex, prog_fd, XDP_FLAGS_DRV_MODE) < 0) {
+        fprintf(stderr, "link set xdp failed\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+    if (setrlimit(RLIMIT_MEMLOCK, &r)) {
+        perror("setrlimit(RLIMIT_MEMLOCK)");
+        return 1;
+    }
+
+    config_t config;
+    config_init(&config);
+
+    FILE *fd = fopen("/etc/compressor/compressor.conf", "r");
+    if (fd) {
+        int res = config_read(&config, fd);
+        if (res == CONFIG_FALSE) {
+            fprintf(stderr, "Error parsing configuration file: %s\n", config_error_text(&config));
+            return 1;
+        }
+        const char *interface;
+        if (config_lookup_string(&config, "interface", &interface) == CONFIG_FALSE) {
+            fprintf(stderr, "Error: No interface defined in configuration file\n");
+            return 1;
+        }
+
+        ifindex = if_nametoindex(interface);
+        if (!ifindex) {
+            perror("Error getting interface");
+            return 1;
+        }
+
+        return load_xdp_prog(argv[0]);
+    } else {
+        perror("Error reading configuration file");
+        return 1;
+    }
+    
+    return 0;
+}
